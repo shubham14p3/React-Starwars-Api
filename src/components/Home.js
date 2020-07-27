cb403f5383a9431b47e029a48a91f1a02d31e138
Shubham Raj <shubham14p3@gmail.com>
2020-07-28 00:21:59 +0530

    added delete button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import {Card,Grid} from 'semantic-ui-react';
-
+import React from "react";
+import {Card, Grid, Button} from "semantic-ui-react";
 
 export default function Home({data}) {
-    return (
-        <>
-        <h1>StartWars</h1>
-        <Grid columns={3}>
-            {data.map((films,i)=>{
-                return(
-                    <Grid.Column key={i}>
-                    <Card>
-                    <Card.Content>
-                        <Card.Header>{films.title}</Card.Header>
-                        <Card.Description>
-                        <strong>Director</strong>
-                        <p>{films.director}</p>
-                        <strong>Release Date</strong>
-                        <p>{films.release_date}</p>
-                        </Card.Description>
-                            </Card.Content>
-                            </Card>
-                            </Grid.Column>
-                        );
-            })}
-        </Grid>
-        </>
-        
-    );
+  return (
+    <>
+      <h1>StartWars</h1>
+      <Grid columns={3}>
+        {data.map((films, i) => {
+          return (
+            <Grid.Column key={i}>
+              <Card>
+                <Card.Content>
+                  <Card.Header>{films.title}</Card.Header>
+                  <Card.Description>
+                    <strong>Director</strong>
+                    <p>{films.director}</p>
+                    <strong>Release Date</strong>
+                    <p>{films.release_date}</p>
+                    <Button primary>Delete</Button>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          );
+        })}
+      </Grid>
+    </>
+  );
 }
